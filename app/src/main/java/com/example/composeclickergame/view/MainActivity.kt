ddcd06d0a00eae09ed6de803b856f953d0dfc93c
Jacob Rein <jakerein@gmail.com>
2023-04-10 11:55:05 -0400

    small changes

--- app/src/main/java/com/example/composeclickergame/view/MainActivity.kt
+++ app/src/main/java/com/example/composeclickergame/view/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import com.example.composeclickergame.ui.theme.ComposeClickerGameTheme
 import dagger.hilt.android.AndroidEntryPoint
 
